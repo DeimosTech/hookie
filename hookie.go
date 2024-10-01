ca421d3792feaede219892531740571dd017aef3
maxhasan882 <rhmithu50@gmail.com>
2024-10-01 13:58:04 +0600

    init

--- hookie.go
+++ hookie.go
@@ -1,10 +1,10 @@
-package main
+package hookie
 
 import (
 	_ "github.com/DeimosTech/hookie/internal/hook"
 	"log/slog"
 )
 
-func main() {
+func init() {
 	slog.Default().Info("hookiee in action")
 }
