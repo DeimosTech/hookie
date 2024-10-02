96b3f05c0718a2a4cffa0458a6448986cb7ddc55
maxhasan882 <rhmithu50@gmail.com>
2024-10-02 16:44:14 +0600

    debug print

--- hookie.go
+++ hookie.go
@@ -1,14 +1,10 @@
 package hookie
 
 import (
-	"github.com/DeimosTech/hookie/db/mongo"
 	_ "github.com/DeimosTech/hookie/internal/hook"
 	"log/slog"
 )
 
 func init() {
-	if mongo.GetDbConnection() == nil {
-		panic("InitMongo should be called")
-	}
 	slog.Default().Info("hookiee in action")
 }
